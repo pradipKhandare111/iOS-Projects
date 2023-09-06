52f9ec6de4bc2584c114dfcb0d0b93f3965b4c76
hassipatel1991 <hasmukh.dobariya@rocketflyer.in>
2023-09-06 18:06:30 +0530

    Initial commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    //IBOutlets allows me to reference UI elements
+    
+    @IBOutlet weak var diceOne: UIImageView!
+    
+    @IBOutlet weak var diceTwo: UIImageView!
+    
+//    var leftDiceNumber = 1
+//    var rightDiceNumber = 5
+    
+    let diceArray = [
+        UIImage(imageLiteralResourceName: "DiceOne"),
+        UIImage(imageLiteralResourceName: "DiceTwo"),
+        UIImage(imageLiteralResourceName: "DiceThree"),
+        UIImage(imageLiteralResourceName: "DiceFour"),
+        UIImage(imageLiteralResourceName: "DiceFive"),
+        UIImage(imageLiteralResourceName: "DiceSix")
+    ]
+    
+    
+    @IBAction func buttonPressed(_ sender: Any)
+    {
+            diceOne.image = diceArray[Int.random(in: 0...5)]
+        
+            diceTwo.image = diceArray[Int.random(in: 0...5)]
+        
+//        leftDiceNumber += 1
+//        rightDiceNumber -= 1
+        
     }
-
 
 }
 

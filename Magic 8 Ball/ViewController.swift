64123a02f88557be00b1a384a33c3c4caeea0238
prtadipkhandare111 <pradip.khandare@neutrinotechlabs.com>
2023-09-08 11:32:21 +0530

    magic 8 ball using random function

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+  
+    @IBOutlet weak var imageViewGame: UIImageView!
+    
+    let arrayBall = [
 
+        UIImage(imageLiteralResourceName: "ball1"),
+        UIImage(imageLiteralResourceName: "ball2"),
+        UIImage(imageLiteralResourceName: "ball3"),
+        UIImage(imageLiteralResourceName: "ball4"),
+        UIImage(imageLiteralResourceName: "ball5")
 
-
+    ]
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+       imageViewGame.image = UIImage(imageLiteralResourceName: "ball3")
+        
+        
+        imageViewGame.image = arrayBall.randomElement()
+//        let rn = arrayBall[Int.random(in: 0...4)]
+//        print(rn)
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+
+//
+
+
+
+
+//
 

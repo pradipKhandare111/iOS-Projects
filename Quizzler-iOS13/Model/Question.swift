89580348d02f5b648181ff159cb94a0c61804696
prtadipkhandare111 <pradip.khandare@neutrinotechlabs.com>
2023-09-13 16:59:56 +0530

    by creating this project I learned about struct/MVC/func/Timer/mutating in struct

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by NTS on 13/09/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct Question {
+    
+    let text : String
+    let answer : String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
